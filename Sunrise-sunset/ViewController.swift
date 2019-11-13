1ec8a376844e1cb0315e590b18e9c0113449c409
Oleh <tyzhai.o@ucu.edu.ua>
2019-11-13 13:21:17 +0200

    Initial Commit

---
+++ Sunrise-sunset/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Sunrise-sunset
+//
+//  Created by Олег on 13.11.2019.
+//  Copyright © 2019 Олег. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
